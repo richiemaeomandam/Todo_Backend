48a44ddd730e2f44a5a314967d7b916022fc7cdf
richiemaeomandam <156775406+richiemaeomandam@users.noreply.github.com>
2025-04-04 16:37:00 +0800

    Update urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
     path('tasks/', views.task_list, name='task_list'),
@@ -7,4 +9,5 @@ urlpatterns = [
     path('tasks/<int:pk>/toggle/', views.toggle_task, name='toggle_task'),
     path('tasks/<int:pk>/delete/', views.delete_task, name='delete_task'),
     path('', views.task_list),  # This maps the base `/todo/` URL to the task list view
+    path('admin/', admin.site.urls),
 ]
