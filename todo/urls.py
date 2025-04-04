21503029ae9c08f9e2e0556279d669234d298baa
richiemaeomandam <156775406+richiemaeomandam@users.noreply.github.com>
2025-04-04 16:14:05 +0800

    Update urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('tasks/add/', views.add_task, name='add_task'),
     path('tasks/toggle/<int:pk>/', views.toggle_task, name='toggle_task'),
     path('tasks/delete/<int:pk>/', views.delete_task, name='delete_task'),
+    path('', views.index, name='todo_index'),
 ]
