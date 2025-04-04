a8ed61d0a8018fc1f9e35f6518e5de97139f32a4
richiemaeomandam <156775406+richiemaeomandam@users.noreply.github.com>
2025-04-04 16:47:49 +0800

    Update urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -12,9 +12,6 @@ urlpatterns = [
     path('', views.task_list),  # This maps the base `/todo/` URL to the task list view
     path('admin/', admin.site.urls),
     path('todo/', include('todo.urls')),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+
     
 ]
