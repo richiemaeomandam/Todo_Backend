52205d57b3be306e46bec09e1ab71d84bf7004e9
richiemaeomandam <156775406+richiemaeomandam@users.noreply.github.com>
2025-04-04 17:15:16 +0800

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todo/', include('todo.urls')),
     path('', lambda request: redirect('todo/', permanent=True)),  # Redirect root to /todo/
+    path('todo/', views.TaskListView.as_view(), name='todo_list'),
 ]
 
