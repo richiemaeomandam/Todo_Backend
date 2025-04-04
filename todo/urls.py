6130d7699ff9429d6fc30c44f514e8163041a4ac
richiemaeomandam <156775406+richiemaeomandam@users.noreply.github.com>
2025-04-04 16:44:49 +0800

    Update urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('tasks/', views.task_list, name='task_list'),
@@ -10,4 +11,10 @@ urlpatterns = [
     path('tasks/<int:pk>/delete/', views.delete_task, name='delete_task'),
     path('', views.task_list),  # This maps the base `/todo/` URL to the task list view
     path('admin/', admin.site.urls),
+    path('todo/', include('todo.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    
 ]
